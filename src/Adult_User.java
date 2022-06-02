a64ba107861c6dd960897cebd8889230cefd2cb5
Antero Júnior <anterojunior1530@gmail.com>
2022-06-01 21:55:42 -0300

    Update Adult_User.java

--- src/Adult_User.java
+++ src/Adult_User.java
@@ -2,7 +2,7 @@ public class Adult_User extends User {
     
     public boolean canBeAdmin;
 
-    Adult_User(String name, String sex, int age, String nickname, String password){
+    Adult_User(String name, String sex, int age, String nickname, String password) throws Exception {
 
         super(name, sex, age, nickname, password);
         this.canBeAdmin = true;
